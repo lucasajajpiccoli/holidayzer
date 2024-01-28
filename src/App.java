686103085ab144d643a89795fd19af0c2e2e3c4b
Lucas Ajaj Piccoli <lucasajaj@gmail.com>
2024-01-28 03:29:27 -0300

    Exercise completed

--- src/App.java
+++ src/App.java
@@ -1,23 +1,46 @@
-public class App {
-    Holiday holiday1 = new Holiday("01-01-2024", "Confraternização Mundial");
-    Holiday holiday2 = new Holiday("12-02-2024", "Carnaval");
-    Holiday holiday3 = new Holiday("13-02-2024", "Carnaval");
-    Holiday holiday4 = new Holiday("29-03-2024", "Sexta-feira Santa");
-    Holiday holiday5 = new Holiday("21-04-2024", "Tiradentes");
-    Holiday holiday6 = new Holiday("01-05-2024", "Dia do Trabalho");
-    Holiday holiday7 = new Holiday("30-05-2024", "Corpus Christi");
-    Holiday holiday8 = new Holiday("07-09-2024", "Independência do Brasil");
-    Holiday holiday9 = new Holiday("12-10-2024", "Nossa Senhora Aparecida");
-    Holiday holiday10 = new Holiday("02-11-2024", "Finados");
-    Holiday holiday11 = new Holiday("15-11-2024", "Proclamação da República");
-    Holiday holiday12 = new Holiday("20-11-2024", "Dia Nacional de Zumbi e da Consciência Negra");
-    Holiday holiday13 = new Holiday("25-12-2024", "Natal");
+import java.util.ArrayList;
+import java.util.List;
 
+public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        fillHolidaysList();
+        printAllHolidays();
+        isDateHoliday("01-01-2024");
+        isDateHoliday("02-01-2024");
+    }
+    
+    static List<Holiday> holidays = new ArrayList<>();
+
+    public static void fillHolidaysList() {
+        holidays.clear();
+        holidays.add(new Holiday("01-01-2024", "Confraternização Mundial"));
+        holidays.add(new Holiday("12-02-2024", "Carnaval"));
+        holidays.add(new Holiday("13-02-2024", "Carnaval"));
+        holidays.add(new Holiday("29-03-2024", "Sexta-feira Santa"));
+        holidays.add(new Holiday("21-04-2024", "Tiradentes"));
+        holidays.add(new Holiday("01-05-2024", "Dia do Trabalho"));
+        holidays.add(new Holiday("30-05-2024", "Corpus Christi"));
+        holidays.add(new Holiday("07-09-2024", "Independência do Brasil"));
+        holidays.add(new Holiday("12-10-2024", "Nossa Senhora Aparecida"));
+        holidays.add(new Holiday("02-11-2024", "Finados"));
+        holidays.add(new Holiday("15-11-2024", "Proclamação da República"));
+        holidays.add(new Holiday("20-11-2024", "Dia Nacional de Zumbi e da Consciência Negra"));
+        holidays.add(new Holiday("25-12-2024", "Natal"));
     }
 
-    public static void printAllHolidays {
-    
+    public static void printAllHolidays () {
+        for(Holiday h: holidays) {
+            System.out.println("Dia " + h.getDate() + " é " + h.getName() + "!");
+        }
+    }
+
+    public static void isDateHoliday(String date) {
+        for(Holiday h: holidays) {
+            if(h.getDate().equals(date)) {
+                System.out.println("Dia " + h.getDate() + " é " + h.getName() + "! 🎉");
+                return;
+            }
+        }
+        System.out.println("Dia " + date + " não é feriado 🥲");
     }
 }
--- src/Holiday.java
+++ src/Holiday.java
@@ -1,46 +1,27 @@
-import java.util.List;
-import java.util.ArrayList;
-
 public class Holiday {
     private String date;
     private String name;
-    private static List<Holiday> holidays = new ArrayList<>();
-    private static List<String> dates = new ArrayList<>();
 
     public Holiday() {}
 
     public Holiday(String date, String name) {
-        if(dates.contains(date)) {
-            return;
-        }
         this.date = date;
         this.name = name;
-        holidays.add(this);
+    }
+
+    public String getDate() {
+        return date;
     }
 
     public void setDate(String date) {
-        if(dates.contains(date)) {
-            return;
-        }
         this.date = date;
     }
 
+    public String getName() {
+        return name;
+    }
+
     public void setName(String name) {
-        if(name.length() == 0) {
-            return;
-        }
         this.name = name;
     }
-
-    public List<Holiday> getAllHolidays() {
-        return holidays;
-    }
-
-    public String getHolidayByDate(String date) {
-        Holiday holidayFromDate = holidays.stream().filter(h -> h.date == date).findAny().orElse(null);
-        if(holidayFromDate == null) {
-            return "";
-        }
-        return holidayFromDate.name;
-    }    
 }
